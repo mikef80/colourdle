eef38123b30eb931d12c0b20f88d879dc560242e
Mike <mike@mike-francis.org>
2022-03-12 13:57:20 +0000

    working on the chart

--- chart.js
+++ chart.js
@@ -7,45 +7,48 @@ if(arr) {
     }
 }
 
-// console.log(counts[1], counts[2], counts[3], counts[4], counts[5]);
-
-
-const labels = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6'
-];
+const labels = ["1", "2", "3", "4", "5", "6"];
 
 const data = {
-    labels: labels,
-    datasets: [{
-        label: 'Guess Distribution',
-        // backgroundColor: 'rgb(255, 99, 132)',
-        backgroundColor: 'rgb(0, 255, 0)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: counts,
-    }]
+  labels: labels,
+  datasets: [
+    {
+      label: "Guess Distribution",
+      data: counts,
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+      ],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+      ],
+      borderWidth: 1,
+    },
+  ],
 };
 
 const config = {
-    type: 'bar',
-    data: data,
-    options: {
-        responsive: true,
-        indexAxis: 'x',
-        scale: {
-            ticks: {
-              precision: 0
-            },
-        },         
-        
-    }
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        title: 'test',
+        beginAtZero: true,
+      },
+    },
+  },
 };
 
-const myChart = new Chart(
-    document.getElementById('myChart'),
-    config
-  );
+const myChart = new Chart(document.getElementById("myChart"), config);
