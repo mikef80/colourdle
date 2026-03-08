84ce96e2dd9ca406e9962e6d4d20b8c9b4c6cee9
Mike <mike@mike-francis.org>
2026-03-08 21:30:54 +0000

    updated colours

--- app.js
+++ app.js
@@ -502,7 +502,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
   function setSwatch() {
     let today = new Date();
-    let yearBegin = new Date(2025, 2, 15); //also changed this value to match date above.  was 2024,2,13
+    let yearBegin = new Date(2026, 2, 8); //also changed this value to match date above.  was 2024,2,13
     let msPassed = today - yearBegin;
     let millisecsInDay = 1000 * 60 * 60 * 24;
     let daysPassed = Math.floor(msPassed / millisecsInDay);
